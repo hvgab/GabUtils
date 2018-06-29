c9ad669ea5f8fae921f11191441d9089ffc1b816
Gabbeh <henrik.v.gabrielsen@gmail.com>
2018-06-29 09:13:45 +0200

    v 0.1.1
    added printing.inpt() and changed version.

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# CHANGELOG
+
+## v.0.1.1
++ Added printing.inpt for question style
+
+## v.0.1
+Pushed to github.
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='gabutils',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     author='Henrik G',
     description='Utils')
